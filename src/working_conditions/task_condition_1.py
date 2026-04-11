2513252bdcaedeb2d4af4bfc728480b0b7331c8a
Daniel Tkachenko <daniltkachenko791@gmail.com>
2026-04-11 13:27:49 +0300

    Corrections were made to the output and a variable was added to convert the type from int to float.

--- src/working_conditions/task_condition_1.py
+++ src/working_conditions/task_condition_1.py
@@ -1,27 +1,31 @@
 order_price = int(input("Введите сумму заказа: "))
 
+# Преобразовани типов с int на float
+float_order_price = float(order_price)
+
+
 print()
 
-if order_price > 10_000:
+if float_order_price > 10_000:
     # размер скидки
     discount = 15 / 100
     # размер скидки в рублях
-    price_discount = round(order_price * discount, 2) # оригинальнее названия для переменной не смог предумать :)
+    price_discount = round(float_order_price * discount, 2) # оригинальнее названия для переменной не смог предумать :)
     # цена с учётом скидки
-    discount_price = round(order_price * (1 - discount), 2)
+    discount_price = round(float_order_price * (1 - discount), 2)
     print(
-        f"Ваша сумма на оплату товара(-ов) составляет: {order_price} рублей.",
+        f"Ваша сумма на оплату товара(-ов) составляет: {float_order_price} рублей.",
         "Размер скидки на товар: 15%.",
         f"Размер скидки на товар в рублях: {price_discount} рублей.",
         f"Итоговая цена на товар с учётом скидки: {discount_price} рублей.",
         sep="\n"
     )
-elif order_price > 5000:
+elif float_order_price > 5_000:
     discount = 10 / 100
-    price_discount = round(order_price * discount, 2)
-    discount_price = round(order_price * (1 - discount), 2)
+    price_discount = round(float_order_price * discount, 2)
+    discount_price = round(float_order_price * (1 - discount), 2)
     print(
-        f"Ваша сумма на оплату товара(-ов) составляет: {order_price} рублей.",
+        f"Ваша сумма на оплату товара(-ов) составляет: {float_order_price} рублей.",
         "Размер скидки на товар: 10%.",
         f"Размер скидки на товар в рублях: {price_discount} рублей.",
         f"Итоговая цена на товар с учётом скидки: {discount_price} рублей.",
@@ -29,10 +33,10 @@ elif order_price > 5000:
     )
 else:
     discount = 5 / 100
-    price_discount = round(order_price * discount, 2)
-    discount_price = round(order_price * (1 - discount), 2)
+    price_discount = round(float_order_price * discount, 2)
+    discount_price = round(float_order_price * (1 - discount), 2)
     print(
-        f"Ваша сумма на оплату: {order_price} рублей.",
+        f"Ваша сумма на оплату товара(-ов) составляет: {float_order_price} рублей.",
         "Размер скидки на товар: 5%.",
         f"Размер скидки на товар в рублях: {price_discount} рублей.",
         f"Итоговая цена на товар с учётом скидки: {discount_price} рублей.",
